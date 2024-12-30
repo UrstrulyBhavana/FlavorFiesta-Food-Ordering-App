d4dfc9455ff656dd5e2a6c07ddbef55a27599b52
Bhavana <154585127+UrstrulyBhavana@users.noreply.github.com>
2024-12-30 10:05:00 +0530

    Add files via upload

---
+++ Grocery.0af20085.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire5906;(0,e.register)("loR3J",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var o=e("ayMG0"),l=()=>(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:" Our Grocery Online Store, and we have a lot of Child Components inside this webpage!!! "})})});
+//# sourceMappingURL=Grocery.0af20085.js.map
