a0f8065bc308d392628babcaba3e9fae36261dcf
Bhavana <154585127+UrstrulyBhavana@users.noreply.github.com>
2024-12-30 10:18:05 +0530

    Delete Grocery.ee39f664.js

--- Grocery.ee39f664.js
+++
@@ -1,2 +0,0 @@
-var e=globalThis.parcelRequire5906;(0,e.register)("loR3J",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var o=e("ayMG0"),l=()=>/*#__PURE__*/(0,o.jsx)("div",{children:/*#__PURE__*/(0,o.jsx)("h1",{children:" Our Grocery Online Store, and we have a lot of Child Components inside this webpage!!! "})})});
-//# sourceMappingURL=Grocery.ee39f664.js.map
